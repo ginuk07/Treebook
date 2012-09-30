77bc846c25419487e562ec7d8da980616ef4797d
Debbie Hoffpauir <it_yogini@gmail.com>
2012-09-30 16:24:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Igopal::Application.config.secret_token = '49553bf33dd3a0d906e79cb85c1ab1e52081d8b2c03dd1b6567f35a6d3f5988d7304737ad879fd9ce94e9c1bb1013a68083be8b2161d8d5b7438305e322d78f1'
